8c6bc9175f124c88edc15f6c72a27c7f376380b8
Brandon Horst <brandonhorst@gmail.com>
2016-01-18 15:17:25 -0500

    Update to include answers

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # apiai-importer
-CLI Tool for importing intents from a CSV file into API.ai
+CLI Tool for importing intents from a CSV file into API.ai. It will also output the Answers as a JSON file that can be imported into Mongo.
 
 ## Installation
 
@@ -13,14 +13,79 @@ cp conf.json.example conf.json
 
 ## Usage
 
-First, download the Intents and Entities sheets from the Google Doc.
+First, download the Intents and Entities sheets from the Google Doc, using the script below.
 
 ```sh
-apiai-import -i ~/Downloads/FAFSA\ -\ Intents.csv -e ~/Downloads/FAFSA\ -\ Entities.csv
+apiai-import -d ~/Google Drive/fafsa*\
 ```
 
 ## Notes
 
 The Google Doc is the Single Source of Truth. This tool will delete all intents and entities from API.ai and regenerate them. Do not make modifications to API.ai directly - instead, change the Google Doc and reimport.
 
+Use the `-e` flag to specify the environment.
+
 This is a public repo, please do not push the keys.
+
+## CSV Download Script
+
+Run this script in the Google Sheets Script Editor to download all sheets to your Google Drive
+
+```js
+function onOpen() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var csvMenuEntries = [{name: "export as csv files", functionName: "saveAsCSV"}];
+  ss.addMenu("csv", csvMenuEntries);
+};
+
+function saveAsCSV() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheets = ss.getSheets();
+  // create a folder from the name of the spreadsheet
+  var folder = DriveApp.createFolder(ss.getName().toLowerCase().replace(/ /g,'_') + '_csv_' + new Date().getTime());
+  for (var i = 0 ; i < sheets.length ; i++) {
+    var sheet = sheets[i];
+    // append ".csv" extension to the sheet name
+    fileName = sheet.getName() + ".csv";
+    // convert all available sheet data to csv format
+    var csvFile = convertRangeToCsvFile_(fileName, sheet);
+    // create a file in the Docs List with the given name and the csv data
+    folder.createFile(fileName, csvFile);
+  }
+  Browser.msgBox('Files are waiting in a folder named ' + folder.getName());
+}
+
+function convertRangeToCsvFile_(csvFileName, sheet) {
+  // get available data range in the spreadsheet
+  var activeRange = sheet.getDataRange();
+  try {
+    var data = activeRange.getValues();
+    var csvFile = undefined;
+
+    // loop through the data in the range and build a string with the csv data
+    if (data.length > 1) {
+      var csv = "";
+      for (var row = 0; row < data.length; row++) {
+        for (var col = 0; col < data[row].length; col++) {
+          data[row][col] = "\"" + data[row][col].replace(/"/g, '""') + "\"";
+        }
+
+        // join each row's columns
+        // add a carriage return to end of each row, except for the last one
+        if (row < data.length-1) {
+          csv += data[row].join(",") + "\r\n";
+        }
+        else {
+          csv += data[row];
+        }
+      }
+      csvFile = csv;
+    }
+    return csvFile;
+  }
+  catch(err) {
+    Logger.log(err);
+    Browser.msgBox(err);
+  }
+}
+```
--- index.js
+++ index.js
@@ -1,208 +1,250 @@
-const _ = require('lodash');
+#!/usr/bin/env node
+
+const _ = require('lodash')
 const pkg = require('./package')
-const async = require('async');
-const conf = require('./conf');
-const fs = require('fs');
-const parse = require('csv-parse');
+const conf = require('./conf')
+const fs = require('fs')
+const parse = require('csv-parse')
+const path = require('path')
 const program = require('commander')
-const request = require('request');
-const through = require('through');
+const request = require('request')
+const thenify = require('thenify')
+const through = require('through')
 const util = require('util')
+
+const readFilePromise = thenify(fs.readFile)
+const writeFilePromise = thenify(fs.writeFile)
+const requestPromise = thenify(request)
+const parsePromise = thenify(parse)
 
 program
   .version(pkg.version)
+  .option('-d, --directory <dir>', 'Specify a directory that contains "Answers.csv", "Entities.csv", and "Intents.csv"')
+  .option('-e, --env <"test"|"dev">', 'specify an environment to import to (as declared in conf.json)')
   .option('-i, --intents <file>', 'Specify an intents CSV file')
   .option('-e, --entities <file>', 'Specify an entities CSV file')
+  .option('-a, --answers <file>', 'Specify an answers CSV file')
+  .option('-o, --answer-output <file>', 'Specificy an output file for the Answers JSON (defaults to ./[epoch]-answers.json')
   .parse(process.argv)
 
-
-
-function cleanEntities (data) {
-  if (!data.name || !data.synonyms) return;
-
-  const synonyms = data.synonyms.split(';')
-
-  return {
-    word: synonyms[0],
-    synonyms: synonyms,
-    name: data.name.substr(1)
+if (program.directory) {
+  program.intents = path.join(program.directory, 'Intents.csv')
+  program.answers = path.join(program.directory, 'Answers.csv')
+  program.entities = path.join(program.directory, 'Entities.csv')
+  if (!program.answerOutput) {
+    program.answerOutput = `${Math.floor(new Date() / 1000)}-answers.json`
   }
 }
 
-function cleanIntents (data) {
-  if (!data.topic || !data.answer || !data.statement) return;
-  const synonyms = data.synonyms ? data.synonyms.split(';') : []
+const parseFunctions = {
+  intents (data) {
+    return parsePromise(data, {
+      columns () {
+        return ['url', 'statement', 'synonyms', 'topic', 'outputContext', 'inputContext']
+      }
+    })
+  },
 
-  return {
-    inputs: [data.statement].concat(synonyms),
-    outputContexts: data.outputContext ? [data.outputContext] : [],
-    inputContexts: data.inputContext ? [data.inputContext] : [],
-    topic: data.topic,
-    answer: data.answer
+  entities (data) {
+    return parsePromise(data, {
+      columns () {
+        return ['name', 'synonyms']
+      }
+    })
+  },
+
+  answers (data) {
+    return parsePromise(data, {
+      columns () {
+        return ['topic', 'answer']
+      }
+    })
   }
 }
 
-function pushIntents (data, done) {
-  const body = {
-    name: data.topic,
-    auto: true,
-    templates: data.inputs,
-    contexts: data.inputContexts,
-    responses: [{
-      resetContexts: !data.outputContexts.length,
-      affectedContexts: data.outputContexts,
-      parameters: [],
-      speech: [data.answer]
-    }]
-  }
+const cleanFunctions = {
+  entities (data) {
+    if (!data.name || !data.synonyms) return
 
-  console.log(`    Importing Intent ${data.topic}`)
+    const synonyms = data.synonyms.split(';')
 
-  apiCall('intents', {method: 'POST', body}, (err, res) => {
-    if (err) {
-      done(err)
-    } else {
-      console.log(`    Imported Intent ${data.topic} as ${res.id}`)
-      done(null)
+    return {
+      word: synonyms[0],
+      synonyms: synonyms,
+      name: data.name.substr(1)
     }
-  })
+  },
+
+  intents (data) {
+    if (!data.topic || !data.statement) return
+    const synonyms = data.synonyms ? data.synonyms.split(';') : []
+
+    return {
+      inputs: [data.statement].concat(synonyms),
+      outputContexts: data.outputContext ? [data.outputContext] : [],
+      inputContexts: data.inputContext ? [data.inputContext] : [],
+      topic: data.topic,
+      answer: data.answer
+    }
+  },
+
+  answers (data) {
+    if (!data.topic || !data.answer) return
+
+    return data
+  }
 }
 
-function pushEntities (data, done) {
-  const body = {
-    name: data.name,
-    entries: [{
-      value: data.word,
-      synonyms: data.synonyms
-    }]
+const pushFunctions = {
+ intents (data) {
+    const pushPromises = _.map(data, item => {
+      const body = {
+        name: item.topic,
+        auto: true,
+        templates: item.inputs,
+        contexts: item.inputContexts,
+        responses: [{
+          action: item.topic,
+          resetContexts: !item.outputContexts.length,
+          affectedContexts: item.outputContexts
+        }]
+      }
+
+      console.log(`    Importing Intent ${item.topic}`)
+
+      return apiCall('intents', {method: 'POST', body}).then((res) => {
+        console.log(`    Imported Intent ${item.topic} as ${res.id}`)
+      })
+    })
+
+    return Promise.all(pushPromises)
+  },
+
+  entities (data) {
+    const pushPromises = _.map(data, item => {
+      const body = {
+        name: item.name,
+        entries: [{
+          value: item.word,
+          synonyms: item.synonyms
+        }]
+      }
+
+      console.log(`    Importing Entity ${item.name}`)
+
+      return apiCall('entities', {method: 'POST', body}).then(res => {
+        console.log(`    Imported Entity ${item.name} as ${res.id}`)
+      })
+    })
+
+    return Promise.all(pushPromises)
+  },
+
+  answers (data) {
+    console.log('    Writing Answers File')
+    return Promise.resolve().then(() => {
+      return writeFilePromise(program.answerOutput, JSON.stringify(data, null, 2))
+    }).then(() => {
+      console.log(`    Wrote Answers File to ${program.answerOutput}`)
+    })
+  }
+}
+
+function apiCall(endpoint, options) {
+  if (!options) options = {}
+
+  const env = program.env || 'dev'
+  if (!conf.env || !conf.env[env] || !conf.env[env].apiai) {
+    console.log('Invalid conf.json file or invalid env specified:', env)
+    return Promise.reject()
   }
 
-  console.log(`    Importing Entity ${data.name}`)
-
-  apiCall('entities', {method: 'POST', body}, (err, res) => {
-    if (err) {
-      done(err)
-    } else {
-      console.log(`    Imported Entity ${data.name} as ${res.id}`)
-      done(null)
-    }
-  })
-}
-
-function apiCall(endpoint, options, done) {
-  request({
+  return requestPromise({
     url: `https://api.api.ai/v1/${endpoint}`,
     method: options.method || 'GET',
     qs: {v: '20150910'},
     body: options.body,
     json: true,
-    auth: {bearer: conf.apiai.access_token},
-    headers: {'ocp-apim-subscription-key': conf.apiai.subscription_key}
-  }, (err, response, body) => {
-    if (err) {
-      done(err)
-    } else {
-      done(null, body)
-    }
+    auth: {bearer: conf.env[env].apiai.access_token},
+    headers: {'ocp-apim-subscription-key': conf.env[env].apiai.subscription_key}
+  }).then(res => {
+    return res[1]
   })
 }
 
-function deleteAll(endpoint, done) {
-  console.log(`** Deleting All ${endpoint} **`)
-  apiCall(endpoint, {}, (err, res) => {
-    if (err) {
-      console.error(err)
-    } else {
-      async.each(res, (item, done) => {
-        console.log(`    Deleting ${endpoint} ${item.name}`)
-        apiCall(`${endpoint}/${item.id}`, {method: 'DELETE'}, (err) => {
-          if (err) {
-            done(err)
-          } else {
-            console.log(`    Deleted ${endpoint} ${item.name}`)
-            done(null)
-          }
-        })
-      }, done)
-    }
+function deleteAll (type) {
+  console.log(`** Deleting All ${_.capitalize(type)} **`)
+
+  return apiCall(type).then(res => {
+    const deletePromises = _.map(res, item => {
+      console.log(`    Deleting ${type} ${item.name}`)
+
+      return apiCall(`${type}/${item.id}`, {method: 'DELETE'}).then(res => {
+        console.log(`    Deleted ${type} ${item.name}`)
+      })
+    })
+
+    return Promise.all(deletePromises)
+  }).then(() => {
+    console.log(`** Deleted All ${_.capitalize(type)} **`)
   })
 }
 
-function parseIntents (data, done) {
-  parse(data, {
-    columns() {return ['url', 'statement', 'synonyms', 'topic', 'data', 'answer', 'outputContext', 'inputContext', 'link', 'additionalInfo'];}
-  }, done)
-}
+function importAll (type) {
+  console.log(`** Importing ${_.capitalize(type)} **`)
 
-function parseEntities (data, done) {
-  parse(data, {
-    columns() {return ['name', 'synonyms'];}
-  }, done)
-}
-
-function importIntents (done) {
-  console.log('** Importing Intents **')
-
-  async.waterfall([
-    (done) => fs.readFile(program.intents, {encoding: 'UTF-8'}, done),
-    parseIntents,
-    (data, done) => done(null, _.chain(data).map(cleanIntents).filter().value()),
-    (data, done) => async.forEach(data, pushIntents, done)
-  ], (err) => {
-    done(err)
-    if (!err) console.log('** Finished Importing Intents **');
+  return readFilePromise(program[type], {encoding: 'UTF-8'}).then(csvData => {
+    return parseFunctions[type](csvData)
+  }).then(data => {
+    return _.chain(data)
+      .map(cleanFunctions[type])
+      .filter()
+      .value()
+  }).then(pushFunctions[type])
+  .then(() => {
+     console.log(`** Finished Importing ${_.capitalize(type)} **`);
   })
 }
 
-function importEntities (done) {
-  console.log('** Importing Entities **')
-
-  async.waterfall([
-    (done) => fs.readFile(program.entities, {encoding: 'UTF-8'}, done),
-    parseEntities,
-    (data, done) => done(null, _.chain(data).map(cleanEntities).filter().value()),
-    (data, done) => async.forEach(data, pushEntities, done)
-  ], (err) => {
-    done(err)
-    if (!err) console.log('** Finished Importing Entities **');
-  })
-}
-
-
-function doDeletions (done) {
-  const toDelete = []
-  if (program.intents) toDelete.push('intents')
-  if (program.entities) toDelete.push('entities')
-
-  async.eachSeries(toDelete, (endpoint, done) => {
-    deleteAll(endpoint, done)
-  }, done)
-}
-
-function doInsertions (done) {
-  const toInsert = []
-  if (program.entities) toInsert.push(importEntities)
-  if (program.intents) toInsert.push(importIntents)
-
-  async.series(toInsert, done)
-}
-
-
-async.waterfall([doDeletions, doInsertions], (err) => {
-  if (err) {
-    console.error(err)
+function getTypes (del) {
+  const types = []
+  if (del) {
+    if (program.intents) types.push('intents')
+    if (program.entities) types.push('entities')
   } else {
-    console.log('**** Done ****')
+    if (program.entities) types.push('entities')
+    if (program.intents) types.push('intents')
+    if (program.answers) types.push('answers')
   }
-})
 
-  // if (err) {
-  //   console.error(err)
-  // } else {
-  //   if (program.intents) {
-  //     importIntents(program.intents)
-  //   }
-  //   if (program.entities) {
-  //     importEntities(program.entities)
-  //   }
-  // }
+  return types
+}
+
+function applyInSeries(types, func) {
+  return _.reduce(types, (promise, type) => {
+    return promise.then(() => func(type))
+  }, Promise.resolve())
+}
+
+function run () {
+  const types = getTypes()
+
+  console.log('**** Starting ****')
+
+  // Order matters here, because we can only delete entities if there are no intents referencing them
+  Promise.resolve().then(() => {
+    return applyInSeries(getTypes(true), deleteAll)
+  }).then(() => {
+    console.log('** Done Deleting **')
+  }).then(() => {
+    return applyInSeries(getTypes(), importAll)
+  }).then(() => {
+    console.log('** Done Inserting **')
+  }).then(() => {
+    console.log('**** Done ****')
+  }).catch(err => {
+    console.error(err)
+  })
+}
+
+run()
